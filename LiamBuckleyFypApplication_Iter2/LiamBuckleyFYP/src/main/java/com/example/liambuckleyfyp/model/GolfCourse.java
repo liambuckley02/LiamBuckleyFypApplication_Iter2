559867050921e24835b1488d36bfe6252def8b9a
liambuckley02 <149475993+liambuckley02@users.noreply.github.com>
2024-11-12 11:42:48 +0000

    Add files via upload

--- LiamBuckleyFypApplication_Iter2/LiamBuckleyFYP/src/main/java/com/example/liambuckleyfyp/model/GolfCourse.java
+++ LiamBuckleyFypApplication_Iter2/LiamBuckleyFYP/src/main/java/com/example/liambuckleyfyp/model/GolfCourse.java
@@ -35,4 +35,69 @@ public class GolfCourse {
     private Double greenFee;
 
     // Getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public Integer getHoles() {
+        return holes;
+    }
+
+    public void setHoles(Integer holes) {
+        this.holes = holes;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Double getGreenFee() {
+        return greenFee;
+    }
+
+    public void setGreenFee(Double greenFee) {
+        this.greenFee = greenFee;
+    }
 }
